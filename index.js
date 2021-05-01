94c42409b1b248a4b81b0149c2b483b2c2db9bc0
NikhilGovind <65320281+NikhilGovind@users.noreply.github.com>
2021-05-01 12:09:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,4 +27,33 @@ $(function(){
             visible: true
         },
     });
+    
+    $("#gridContainer1").dxDataGrid({
+        dataSource: "data/needhelp.json",
+        columns: [
+            "State", 
+            {dataField : "District", visible: false},
+            {dataField : "City", visible: false}, 
+            {dataField : "Pincode", visible: false}, 
+            "Name", 
+            "Condition", 
+            {dataField : "Phone", visible: false}, 
+            "Emergency", 
+            {dataField : "Status", visible: false},
+            {dataField : "Priority", visible: false}
+        ],
+        showBorders: true,
+        paging: {
+            pageSize: 10
+        },  
+        groupPanel: {
+            visible: true
+        },
+        grouping: {
+            autoExpandAll: true,
+        },
+        searchPanel: {
+            visible: true
+        },
+    });
 });
