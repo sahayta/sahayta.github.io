20a5ed38b148ad8c9926be6a212da1c0602b27cf
NikhilGovind <65320281+NikhilGovind@users.noreply.github.com>
2021-05-01 12:04:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(function(){
             "Condition", 
             "Phone", 
             "Emergency", 
-            "Status", 
+            {dataField : "Status", , visible: false}
             {dataField : "Priority", visible: false}
         ],
         showBorders: true,
