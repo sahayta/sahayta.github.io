78549de61d5db56a9571939e5d452e4935ff65c8
NikhilGovind <65320281+NikhilGovind@users.noreply.github.com>
2021-05-01 11:24:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(function(){
     $("#gridContainer").dxDataGrid({
-        dataSource: "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/data/customers.json",
+        dataSource: "data/customers.json",
         columns: ["CompanyName", "City", "State", "Phone", "Fax"],
         showBorders: true
     });
